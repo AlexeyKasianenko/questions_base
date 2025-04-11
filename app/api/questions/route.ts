b9393f8853a20672e31ad146dd8013405526f614
Olexii Kasianenko <olexiy.k@didww.com>
2025-04-11 17:33:09 +0300

    ``` Add category and rank filters to questions API and UI
    - Updated API to support filtering by category and rank.
    - Modified UI to include dropdowns for category and rank filters.
    - Adjusted fetch logic to include new filter parameters.
    ```

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -6,15 +6,31 @@ export async function GET(req: NextRequest) {
   const per_page = 10;
   const page = parseInt(req.nextUrl.searchParams.get("_page") as string) | 1;
   const order = (req.nextUrl.searchParams.get("order_by") as string) || "";
+  const category = req.nextUrl.searchParams.get("category") as string;
+  const rank = req.nextUrl.searchParams.get("rank") as string;
   const offset = (page - 1) * per_page;
+  let whereClause = "where ";
+  if (category !== "all") {
+    whereClause += `category = '${category}' `;
+  }
+  if (rank !== "all") {
+    const and = whereClause == "where " ? "" : "and ";
+
+    whereClause += `${and}rank = '${rank}' `;
+  }
+  if (category === "all" && rank === "all") {
+    whereClause = "";
+  }
+
   const questions = await db.all(
-    `select * from questions order by ${order} limit ? offset ?`,
+    `select *, count(*) over () as total_questions from questions ${whereClause}order by ${order} limit ? offset ?`,
     [per_page, offset],
   );
-  const { total } = await db.get("select count(*) as total from questions");
+  const total = questions.length > 0 ? questions[0].total_questions : 0;
   const response = {
     data: questions,
     pages: Math.ceil(total / per_page),
   };
+
   return NextResponse.json(response);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import { redirect, RedirectType } from "next/navigation";
-import { Box, display } from "@mui/system";
+import { Box } from "@mui/system";
 import Pagination from "@mui/material/Pagination";
 import ReactMarkdown from "react-markdown";
 import { useSearchParams } from "next/navigation";
@@ -26,9 +26,13 @@ export default function Home() {
   const searchParams = useSearchParams();
   const currentPage = parseInt(searchParams.get("page") || "1");
   const orderBy = searchParams.get("order_by") || "id";
+  const rank = searchParams.get("rank") || "all";
+  const category = searchParams.get("category") || "all";
 
   async function fetchQuestions(page: number = 1) {
-    const res = await fetch(`/api/questions?_page=${page}&order_by=${orderBy}`);
+    const res = await fetch(
+      `/api/questions?_page=${page}&order_by=${orderBy}&category=${category}&rank=${rank}`,
+    );
     if (!res.ok) {
       throw new Error("Failed to fetch questions");
     }
@@ -53,40 +57,98 @@ export default function Home() {
       setTotalPages(response.pages);
       setQuestions(response.data);
     });
-  }, [currentPage, orderBy]);
+  }, [currentPage, orderBy, category, rank]);
 
   const handlePageChanging = (event: BaseSyntheticEvent) => {
     redirect(
-      `http://localhost:3000?page=${event.target.innerText}&order_by=${orderBy}`,
+      `http://localhost:3000?page=${event.target.innerText}&order_by=${orderBy}&category=${category}&rank=${rank}`,
     );
   };
 
   const handleOrderByChanging = (event: BaseSyntheticEvent) => {
     redirect(
-      `http://localhost:3000?page=${currentPage}&order_by=${event.target.value}`,
+      `http://localhost:3000?page=${currentPage}&order_by=${event.target.value}&category=${category}&rank=${rank}`,
+    );
+  };
+
+  const handleRankFilterChanging = (event: BaseSyntheticEvent) => {
+    redirect(
+      `http://localhost:3000?page=${1}&order_by=${orderBy}&category=${category}&rank=${event.target.value}`,
+    );
+  };
+
+  const handleCategoryFilterChanging = (event: BaseSyntheticEvent) => {
+    redirect(
+      `http://localhost:3000?page=${1}&order_by=${orderBy}&category=${event.target.value}&rank=${rank}`,
     );
   };
 
   return (
     <div>
-      <form className="container mx-auto mt-10">
-        <label
-          htmlFor="order_by"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Order by
-        </label>
-        <select
-          id="order_by"
-          name="order_by"
-          value={orderBy}
-          onChange={(e) => handleOrderByChanging(e)}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          <option value="id">Order by</option>
-          <option value="rank">Rank</option>
-          <option value="category">Category</option>
-        </select>
+      <form className="container mx-auto mt-10 columns-3">
+        <div>
+          <label
+            htmlFor="order_by"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Order by
+          </label>
+          <select
+            id="order_by"
+            name="order_by"
+            value={orderBy}
+            onChange={(e) => handleOrderByChanging(e)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="id">Order by</option>
+            <option value="rank">Rank</option>
+            <option value="category">Category</option>
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="rank"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Rank filter
+          </label>
+          <select
+            id="rank"
+            name="rank"
+            value={rank}
+            onChange={(e) => handleRankFilterChanging(e)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="all">All</option>
+            {Object.values(Rank).map((r) => (
+              <option key={r} value={r}>
+                {r}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="category"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Category filter
+          </label>
+          <select
+            id="category"
+            name="category"
+            value={category}
+            onChange={(e) => handleCategoryFilterChanging(e)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="all">All</option>
+            {Object.values(Category).map((cat) => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </div>
       </form>
       <div className="container mx-auto mt-10">
         {questions.map((question: Question) => (
